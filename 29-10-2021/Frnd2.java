6799818d6489a7ed8f05e1d4f2a90ec30c5846cc
Sandesha Naik <sandeshnaik215@gmail.com>
2021-10-30 10:42:53 +0530

    updated

--- 29-10-2021/Frnd2.java
+++ 29-10-2021/Frnd2.java
@@ -4,12 +4,29 @@ public class Frnd2 {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Hello\nCan you give me rani's Number : ");
-	long l=sc.nextLong();
-	if(l>6000000000l &&l<9999999999l) {
-		 System.out.println("bro this number is not reachable");
+	long l;
+	try {
+		l = sc.nextLong();
+		if(l>6000000000l &&l<9999999999l) {
+			 System.out.println("bro this number is not reachable");
+			 long m=sc.nextLong();
+			if(l>6000000000l &&l<9999999999l) {
+			System.out.println("bro this number also not reachable bro");
+			 {
+				System.out.println("thank you bro /n i will massage you later");
+			}
+					}
+		}
+	} catch (Exception e) {
+		System.out.println("invailed input");
+		e.getMessage();
 	}
-	else {
-		System.out.println("enter the Valid Mobile Number");
-	}
+
+	
+
+	
+
 }
 }
+
+
